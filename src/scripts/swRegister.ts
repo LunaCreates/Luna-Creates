b5d80cf91844581969601ae1528c3b163cc3457b
Alex Clapperton <hi@alexclapperton.co.uk>
2020-04-12 12:45:34 +0100

    ✨ Add service worker

---
+++ src/scripts/swRegister.ts
@@ -0,0 +1,13 @@
+function swRegister() {
+  function init() {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js').catch(error => console.log(error));
+    }
+  }
+
+  return {
+    init
+  }
+}
+
+export default swRegister();
