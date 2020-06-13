ce72bcb32d78ae6024426ef5e9ded8c1bc7565a4
Alex Clapperton <hi@alexclapperton.co.uk>
2020-06-13 19:07:49 +0100

    ✨ Add key map script

---
+++ src/scripts/shopify/keyMap.ts
@@ -0,0 +1,57 @@
+import pubSub from '../modules/pubSub';
+
+function KeyMap(product: HTMLElement) {
+  const modal: HTMLDialogElement | null = product.querySelector('[data-key-map-modal]');
+  const keyMapContainer: HTMLElement | null = product.querySelector('[data-key-map]');
+
+  function showKeyMapModal() {
+    const basketButton: HTMLButtonElement = modal?.querySelector('[data-add-to-basket]') as HTMLButtonElement;
+
+    modal?.removeAttribute('hidden');
+    basketButton.textContent = 'Add key map to basket';
+    basketButton.removeAttribute('disabled');
+    basketButton.focus();
+  }
+
+  function renderKeyMapImage(keyMap: string) {
+    const keyMapImage = `<img src="${keyMap}" class="modal__key-map-image" alt="Preview image of personalised key map">`;
+
+    keyMapContainer?.insertAdjacentHTML('afterbegin', keyMapImage);
+  }
+
+  function closeModal() {
+    if (!keyMapContainer) return;
+
+    modal?.setAttribute('hidden', '');
+    keyMapContainer.innerHTML = '';
+  }
+
+  function handleClickEvent(event: Event) {
+    if (!(event.target instanceof HTMLButtonElement)) return;
+
+    if (event.target.hasAttribute('data-close-modal')) {
+      closeModal();
+    }
+  }
+
+  function handleKeyUpEvent(event: KeyboardEvent) {
+    if (event.key === 'Escape') {
+      closeModal();
+    }
+  }
+
+  function init() {
+    if (modal && keyMapContainer) {
+      pubSub.subscribe('show/key/map/modal', showKeyMapModal);
+      pubSub.subscribe('key/map/created', renderKeyMapImage);
+      modal.addEventListener('click', handleClickEvent);
+      modal.addEventListener('keyup', handleKeyUpEvent);
+    }
+  }
+
+  return {
+    init
+  }
+}
+
+export default KeyMap;
