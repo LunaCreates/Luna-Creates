c2090ae6bd18d63c6a019a1815c4344ee045a912
Alex Clapperton <hi@alexclapperton.co.uk>
2020-06-13 19:04:22 +0100

    🎨 Update products data

--- src/site/_data/products.js
+++ src/site/_data/products.js
@@ -11,7 +11,7 @@ async function allProductsData() {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      Accept: 'application/json',
+      'Accept': 'application/json',
       'X-Shopify-Storefront-Access-Token': `${token}`
     },
     body: JSON.stringify({
@@ -19,16 +19,29 @@ async function allProductsData() {
         products(first: 50, query: "available_for_sale:true") {
           edges {
             node {
+              id
               title
               handle
               descriptionHtml
-              options {
-                values
-              }
+              tags
               priceRange {
                 minVariantPrice {
                   amount
                   currencyCode
+                }
+              }
+              variants(first: 50) {
+                edges {
+                  node {
+                    id
+                    selectedOptions {
+                      name,
+                      value
+                    }
+                    priceV2 {
+                      amount
+                    }
+                  }
                 }
               }
               images(first: 10) {
@@ -59,6 +72,19 @@ async function allProductsData() {
   // get data from the JSON response
   const products = response.data.products.edges.map(edge => edge.node);
 
+  function getColorFromTags(tags) {
+    return tags.filter(tag => tag.toLowerCase().match(/black|white|grey/));
+  }
+
+  function formatOptions(option) {
+    return {
+      id: option.node.id,
+      name: option.node.selectedOptions[0].name,
+      value: option.node.selectedOptions[0].value,
+      price: option.node.priceV2.amount
+    }
+  }
+
   // format all product images
   function formatProductImages(image) {
     return {
@@ -69,15 +95,19 @@ async function allProductsData() {
 
   // format products objects
   const productsFormatted = products.map(item => {
+    const color = getColorFromTags(item.tags);
+    const options = item.variants.edges.map(formatOptions);
     const images = item.images.edges.map(formatProductImages);
     const price = item.priceRange.minVariantPrice.amount;
     const currency = item.priceRange.minVariantPrice.currencyCode;
 
     return {
+      id: item.id,
       title: item.title,
       slug: item.handle,
       description: item.descriptionHtml,
-      options: item.options[0].values,
+      color: color,
+      options: options,
       mainImageAlt: item.images.edges[0].node.altText,
       mainImage: item.images.edges[0].node.originalSrc.split('.jpg')[0],
       thumbnails: images,
