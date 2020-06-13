cd678829c1bdf64d31a016d85dda8f1a0f3dfb07
Alex Clapperton <hi@alexclapperton.co.uk>
2020-06-13 19:08:14 +0100

    ✨ Add shopify script

---
+++ src/scripts/shopify/shopify.ts
@@ -0,0 +1,23 @@
+import checkout from './checkout';
+import form from './form';
+import price from './price';
+import variants from './variants';
+import keyMap from './keyMap';
+import basket from './basket';
+
+function Shopify(product: HTMLElement) {
+  function init() {
+    checkout(product).init();
+    form(product).init();
+    price(product).init();
+    basket(product).init();
+    variants(product).init();
+    keyMap(product).init();
+  }
+
+  return {
+    init
+  }
+}
+
+export default Shopify;
