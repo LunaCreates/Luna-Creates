b361ca15d1c7314e3cb35c9a2ee86c3660452d43
Alex Clapperton <hi@alexclapperton.co.uk>
2020-06-13 19:07:11 +0100

    ✨ Add checkout script

---
+++ src/scripts/shopify/checkout.ts
@@ -0,0 +1,51 @@
+import shopify from '../modules/shopify';
+
+function Checkout(product: HTMLElement) {
+  const existingCheckoutId = localStorage.getItem('shopify_checkout_id');
+
+  function createCheckout() {
+    shopify.checkout.create().then(checkout => {
+      localStorage.setItem('shopify_checkout_id', `${checkout.id}`);
+    });
+  }
+
+  function updateCartCount(count: number) {
+    const cart = Array.from(document.querySelectorAll('[data-cart]'));
+
+    console.log(cart, 'updateCartCount');
+
+    cart.forEach(item => item.setAttribute('data-count', `${count}`));
+  }
+
+  function fetchExistingCheckout() {
+    const checkoutId  = existingCheckoutId as string;
+
+    shopify.checkout.fetch(checkoutId).then(checkout => {
+      const items = checkout.lineItems;
+
+      console.log(checkout, 'fetchExistingCheckout');
+
+      if (items.length > 0) {
+        updateCartCount(items.length);
+      }
+    });
+  }
+
+  function checkForCheckout() {
+    if (!existingCheckoutId) {
+      createCheckout();
+    } else {
+      fetchExistingCheckout();
+    }
+  }
+
+  function init() {
+    checkForCheckout();
+  }
+
+  return {
+    init
+  }
+}
+
+export default Checkout;
