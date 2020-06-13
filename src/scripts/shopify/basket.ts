2d34d528f9bc23bf1fd9de726b8dad3fc7cead83
Alex Clapperton <hi@alexclapperton.co.uk>
2020-06-13 19:07:01 +0100

    ✨Add basket script

---
+++ src/scripts/shopify/basket.ts
@@ -0,0 +1,64 @@
+import pubSub from '../modules/pubSub';
+import shopify from '../modules/shopify';
+
+function Basket(product: HTMLElement) {
+  const checkoutId = localStorage.getItem('shopify_checkout_id');
+  const basketButton = product.querySelector('[data-add-to-basket]');
+
+  function setAttributes(varaint: HTMLOptionElement) {
+    const key = varaint.getAttribute('data-name');
+    const value = varaint.getAttribute('value');
+
+    return { key, value };
+  }
+
+  function updateBasketButton(varaint: HTMLOptionElement) {
+    const variantId = varaint.getAttribute('data-id');
+    const customAttributes = JSON.stringify(setAttributes(varaint));
+
+    if (!basketButton || !variantId) return;
+
+    basketButton.setAttribute('data-variant-id', variantId);
+    basketButton.setAttribute('data-variant-options', customAttributes);
+  }
+
+  function updateBasketCount(checkout: ShopifyBuy.Cart) {
+    const cart = Array.from(document.querySelectorAll('[data-cart]'));
+    const itemCount = checkout.lineItems.length;
+
+    cart.forEach(item => item.setAttribute('data-count', `${itemCount}`));
+  }
+
+  function updateBasket(event: Event) {
+    if (!(event.target instanceof HTMLButtonElement) || !checkoutId || !basketButton) return;
+
+    const id = event.target.getAttribute('data-variant-id');
+    const attributes = event.target.getAttribute('data-variant-options');
+    const lineItemsToAdd: any = [
+      {
+        variantId: id,
+        quantity: 1,
+        customAttributes: [JSON.parse(attributes as string)]
+      }
+    ];
+
+    console.log(lineItemsToAdd, 'lineItemsToAdd');
+
+    basketButton.setAttribute('disabled', 'true');
+    basketButton.textContent = 'Added';
+
+    shopify.checkout.addLineItems(checkoutId, lineItemsToAdd)
+      .then(checkout => updateBasketCount(checkout));
+  }
+
+  function init() {
+    pubSub.subscribe('variant/changed', updateBasketButton);
+    basketButton?.addEventListener('click', updateBasket);
+  }
+
+  return {
+    init
+  }
+}
+
+export default Basket;
