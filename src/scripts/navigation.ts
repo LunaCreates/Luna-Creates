03d6189f1daa68b7455d1deeb860e2ce67e9870c
Alex Clapperton <hi@alexclapperton.co.uk>
2020-08-31 12:16:48 +0100

    🐛 Fix sub nav issue on tablet

--- src/scripts/navigation.ts
+++ src/scripts/navigation.ts
@@ -4,27 +4,29 @@ function Naviagtion(nav: HTMLElement) {
   const resiveObserve = new ResizeObserver(resizeCallback);
   const navButton = nav.querySelector('[data-nav-button]') as HTMLButtonElement;
   const navList = nav.querySelector('[data-nav-list]') as HTMLUListElement;
+  const navSub = nav.querySelector('[data-sub-nav]') as HTMLUListElement;
   const navShopItem = nav.querySelector('[data-nav-item="Shop"]') as HTMLAnchorElement;
 
   function toggleSubNav(event: TouchEvent) {
     const target = event.target as HTMLAnchorElement;
     const isExpanded: boolean = target.getAttribute('aria-expanded') === 'true';
-    const navSub = nav.querySelector('[data-sub-nav]') as HTMLUListElement;
     const navListHeight = navList.scrollHeight;
     const navSubHeight = navSub.scrollHeight;
     const position = isExpanded ? navListHeight - navSubHeight : navListHeight + navSubHeight;
 
     event.preventDefault();
 
-    navSub.style.height = `${isExpanded ? '0' : navSubHeight}px`;
     target.setAttribute('aria-expanded', `${!isExpanded}`);
     pubSub.publish('nav/visibility/changed', position + 1);
+
+    if (window.outerWidth >= 768) return;
+
+    navSub.style.height = `${isExpanded ? '0' : navSubHeight}px`;
   }
 
   function resizeCallback(entries: Array<ResizeObserverEntry>) {
     entries.forEach(entry => {
       const width = entry.contentRect.width;
-      const navListHasOpenClass = navList.classList.contains('nav__list--open');
 
       if ('ontouchstart' in document.documentElement) {
         navShopItem.setAttribute('aria-expanded', 'false');
@@ -34,11 +36,9 @@ function Naviagtion(nav: HTMLElement) {
         navShopItem.removeEventListener('touchend', toggleSubNav);
       }
 
-      if (width >= 768 && navListHasOpenClass) {
+      if (width >= 768) {
+        navSub.removeAttribute('style');
         navList.classList.remove('nav__list--open');
-        resiveObserve.unobserve(document.body);
-      } else {
-        resiveObserve.observe(document.body);
       }
     });
   }
@@ -62,7 +62,7 @@ function Naviagtion(nav: HTMLElement) {
     isExpanded ? closeNav(target) : openNav(target);
   }
 
-  function init () {
+  function init() {
     navButton.addEventListener('click', handleClickEvent);
     resiveObserve.observe(document.body);
   }
