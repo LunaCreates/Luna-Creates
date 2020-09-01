580c9d7778563691669960fb7d218eed66d51152
Alex Clapperton <hi@alexclapperton.co.uk>
2020-09-01 14:36:20 +0100

    🚧  Update order lambda to log item properties

--- src/functions/order.js
+++ src/functions/order.js
@@ -1,5 +1,6 @@
 exports.handler = async (event, context, callback) => {
   const data = JSON.parse(event.body);
+  const items = data.line_items[0].properties;
 
-  console.log(data, 'order');
+  console.log(items, 'order');
 }
