fe97757b17a840a49698ca8abac78458a83e38b3
Alex Clapperton <hi@alexclapperton.co.uk>
2020-09-03 16:18:50 +0100

    🚧  Begin refactor of image zoom

--- src/scripts/imageZoom.ts
+++ src/scripts/imageZoom.ts
@@ -1,141 +1,64 @@
 import pubSub from './modules/pubSub';
 
-function ImageZoom(element: HTMLElement) {
-  const original = element.querySelector('[data-product-image]') as HTMLElement;
-  const magnified = element.querySelector('[data-image-zoom]') as HTMLElement;
-  let isActive = false;
+function ImageZoom(product: HTMLElement) {
+  const button: HTMLButtonElement | null = product.querySelector('[data-image-zoom]');
+  const modal: HTMLElement | null = product.querySelector('[data-modal="image-zoom"]');
 
-  function setImageZoomSize() {
-    const width = original.getBoundingClientRect().width * 3;
-    const height = original.getBoundingClientRect().height * 3;
+  function showImageZoomModal(module: any) {
+    const imageZoomModal = module.default;
 
-    magnified.style.width = `${width}px`;
-    magnified.style.height = `${height}px`;
+    if (modal === null) return;
+
+    imageZoomModal(modal).openModal(button);
+  }
+
+  function runDesktopImageZoom(module: any) {
+    const desktopImageZoom = module.default;
+
+    desktopImageZoom(modal).init();
+  }
+
+  function handleResizeEntry(entry: ResizeObserverEntry) {
+    const isTouchDevice = 'ontouchstart' in document.documentElement;
+
+    if (isTouchDevice) {
+      console.log('Touch device');
+    } else {
+      import('./desktopImageZoom').then(runDesktopImageZoom);
+    }
   }
 
   function resizeCallback(entries: Array<ResizeObserverEntry>) {
-    entries.forEach((entry: ResizeObserverEntry) => {
-      if (entry.contentRect) {
-        setImageZoomSize();
-      }
-    });
+    entries.forEach(handleResizeEntry);
   }
 
-  function drawMask(x: number, y: number) {
-    const image = original.getBoundingClientRect();
-    const imageZoom = magnified.getBoundingClientRect();
-    const propX = x / image.width * imageZoom.width * (1 - 1 / 3) - image.x;
-    const propY = y / image.height * imageZoom.height * (1 - 1 / 3) - image.y;
-    const maskX = x * 3;
-    const maskY = y * 3;
-    const clip = `circle(150px at ${maskX}px ${maskY}px)`;
-
-    magnified.style.left = `${-propX}px`;
-    magnified.style.top = `${-propY}px`;
-    magnified.style.opacity = '1';
-    magnified.style.clipPath = clip;
+  function handleImageZoom() {
+    const resiveObserve = new ResizeObserver(resizeCallback);
+    resiveObserve.observe(document.body);
   }
 
-  function setZoom(x: number, y: number) {
-    const bg = magnified.getAttribute('data-bg');
+  function handleClick() {
+    if (button === null) return;
 
-    if (magnified.hasAttribute('data-bg')) {
-      magnified.style.backgroundImage = `url(${bg})`;
-      magnified.removeAttribute('data-bg');
-    }
-
-    if (!isActive) return;
-
-    drawMask(x, y);
-  }
-
-  function handleMove(posX: number, posY: number) {
-    const top = original.getBoundingClientRect().top;
-    const left = original.getBoundingClientRect().left;
-    const x = (posX - left) - window.pageXOffset;
-    const y = (posY - top) - window.pageYOffset;
-
-    setZoom(x, y);
-  }
-
-  function handleStart(posX: number, posY: number) {
-    const top = original.getBoundingClientRect().top;
-    const left = original.getBoundingClientRect().left;
-    const x = (posX - left) - window.pageXOffset;
-    const y = (posY - top) - window.pageYOffset;
-
-    isActive = true;
-    drawMask(x, y);
-  }
-
-  function handleMoveEnd() {
-    isActive = false;
-    magnified.style.opacity = '0';
-  }
-
-  function handleMouseMove(event: MouseEvent) {
-    const posX = event.pageX;
-    const posY = event.pageY;
-
-    event.preventDefault();
-
-    handleMove(posX, posY);
-  }
-
-  function handleMouseStart(event: MouseEvent) {
-    const posX = event.pageX;
-    const posY = event.pageY;
-
-    magnified.style.display = 'block';
-
-    handleStart(posX, posY);
-  }
-
-  function handleTouchMove(event: TouchEvent) {
-    const posX = event.changedTouches[0].pageX;
-    const posY = event.changedTouches[0].pageY;
-
-    event.preventDefault();
-
-    handleMove(posX, posY);
-  }
-
-  function handleTouchStart(event: TouchEvent) {
-    const posX = event.changedTouches[0].pageX;
-    const posY = event.changedTouches[0].pageY;
-
-    magnified.style.display = 'block';
-
-    handleStart(posX, posY);
-  }
-
-  function addEventListeners() {
-    element.addEventListener('mousemove', handleMouseMove);
-    element.addEventListener('mousedown', handleMouseStart, { passive: true });
-    element.addEventListener('mouseup', handleMoveEnd, { passive: true });
-
-    element.addEventListener('touchmove', handleTouchMove);
-    element.addEventListener('touchstart', handleTouchStart, { passive: true });
-    element.addEventListener('touchend', handleMoveEnd, { passive: true });
+    import('./modules/modal')
+      .then(showImageZoomModal)
+      .then(handleImageZoom);
   }
 
   function changeImageZoomSrc(picture: HTMLPictureElement) {
-    const image = picture.querySelector('img')?.srcset.split('_75x75');
+    const imagePath = picture.querySelector('img')?.srcset.split('_75x75')[0] as string;
+    const image = modal?.querySelector('[data-product-image]') as HTMLImageElement;
 
-    if (!image || !magnified) return;
+    if (image === null && imagePath === undefined) return;
 
-    magnified.style.backgroundImage = `url(${image[0]}.jpg)`;
+    image.srcset = `${imagePath}.jpg`;
   }
 
   function init() {
-    const resizeObserver = new ResizeObserver(resizeCallback);
+    if (button === null) return;
 
-    if (element !== null) {
-      setImageZoomSize();
-      addEventListeners();
-      resizeObserver.observe(original);
-      pubSub.subscribe('main/product/image/changed', changeImageZoomSrc);
-    }
+    button.addEventListener('click', handleClick);
+    pubSub.subscribe('main/product/image/changed', changeImageZoomSrc);
   }
 
   return {
