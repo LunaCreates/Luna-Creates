909888d124fe93b17869aa0500da300b962941a0
Alex Clapperton <hi@alexclapperton.co.uk>
2020-11-03 20:03:30 +0000

    🚧Add post layout

--- .eleventy.js
+++ .eleventy.js
@@ -4,15 +4,22 @@ const htmlMin = require('./src/utils/minify-html.js');
 const swStyles = require('./src/utils/filters/sw-styles.js');
 const swScripts = require('./src/utils/filters/sw-scripts.js');
 const swBuildNumber = require('./src/utils/filters/sw-build-number.js');
+const settings = require('./src/site/_data/settings.json');
 
 module.exports = config => {
   const prod = process.env.NODE_ENV === 'prod';
+  const now = new Date();
+  const livePosts = post => post.date <= now && !post.data.draft;
 
+  // Filters
   config.addFilter('formatDate', formatDate);
+
+  // Shortcodes
   config.addShortcode('swStyles', swStyles);
   config.addShortcode('swScripts', swScripts);
   config.addShortcode('swBuildNumber', swBuildNumber);
 
+  // Passthrough copy
   config.addPassthroughCopy({ 'src/favicons': 'favicons' });
   config.addPassthroughCopy({ 'src/fonts': 'fonts' });
   config.addPassthroughCopy({ 'src/images': 'images' });
@@ -24,6 +31,14 @@ module.exports = config => {
     config.addTransform('htmlmin', htmlMin);
   }
 
+  // Custom collections
+  config.addCollection('postFeed', collection => {
+    return [...collection.getFilteredByGlob('./src/posts/*.md').filter(livePosts)]
+      .reverse()
+      .slice(0, settings.maxPostsPerPage);
+  });
+
+  // 404
   config.setBrowserSyncConfig({
     callbacks: {
       ready: function (err, bs) {
