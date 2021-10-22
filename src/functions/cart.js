36ac1687ba283d52566f3e6a8880d42ad6da6ece
Alex Clapperton <hi@alexclapperton.co.uk>
2021-10-23 00:50:15 +0100

    🎨 Update root url in cart function for deploy previews

--- src/functions/cart.js
+++ src/functions/cart.js
@@ -162,10 +162,7 @@ async function createCheckout(cart, clientId, rootURL) {
 }
 
 exports.handler = async function (event, context, callback) {
-  const fromPR = process.env.PULL_REQUEST === 'true'
-  const rootURL = fromPR ? process.env.DEPLOY_URL : process.env.URL || 'https://localhost:8888';
-
-  console.log(JSON.stringify(process.env), 'process.env');
+  const rootURL = process.env.URL || 'https://localhost:8888';
 
   try {
     const data = JSON.parse(event.body);
